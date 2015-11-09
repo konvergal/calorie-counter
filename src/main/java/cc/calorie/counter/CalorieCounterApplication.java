251995d5055a8f03646d0e002a94dc98c70357c4
Antal Kiss <konvergal@gmail.com>
2015-11-09 15:21:58 +0100

    Initial project structure.

---
+++ src/main/java/cc/calorie/counter/CalorieCounterApplication.java
@@ -0,0 +1,16 @@
+package cc.calorie.counter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by konvergal on 28/10/15.
+ */
+@SpringBootApplication
+public class CalorieCounterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CalorieCounterApplication.class, args);
+    }
+
+}
